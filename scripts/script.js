b3b0e3e57181a47911dd7be7ddc0b4ee40dbba77
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 07:51:57 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
 const { ethers } = require("ethers");
 const hardhat = require("hardhat");
 
+const Wrapped = require("../artifacts/contracts/WrappedEther.sol/WrappedEther.json");
+
 async function main() {
+  console.log(Wrapped.abi);
   // We get the contract to deploy
-  const WrappedEther = await hardhat.ethers.getContractFactory("WrappedEther");
-  const wrappedEther = await WrappedEther.deploy();
-
-  await wrappedEther.deployed();
-
-  console.log("WrappedEther deployed to:", wrappedEther.address);
+  // const WrappedEther = await hardhat.ethers.getContractFactory("WrappedEther");
+  // const wrappedEther = await WrappedEther.deploy();
+  // await wrappedEther.deployed();
+  // console.log("WrappedEther deployed to:", wrappedEther.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
