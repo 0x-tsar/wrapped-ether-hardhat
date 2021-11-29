cdace0ee25a60f84a29d0d78ec1a97a31931df6b
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-29 08:08:47 -0300

    :sparkles:

--- scripts/script.js
+++ scripts/script.js
@@ -5,31 +5,31 @@ const hardhat = require("hardhat");
 const Wrapped = require("../artifacts/contracts/WrappedEther.sol/WrappedEther.json");
 
 async function main() {
-  console.log(Wrapped.abi);
-  // We get the contract to deploy
+  const provider = new ethers.providers.JsonRpcProvider(process.env.url);
+
+  const wrappedEther = new ethers.Contract(
+    "0xa91ca5922c21dfec758f0467cc296b49a31ded35",
+    Wrapped.abi,
+    provider
+  );
+
+  const ticker = await wrappedEther.symbol();
+  const name = await wrappedEther.name();
+
+  console.log(name);
+  console.log(ticker);
+
   // const WrappedEther = await hardhat.ethers.getContractFactory("WrappedEther");
   // const wrappedEther = await WrappedEther.deploy();
   // await wrappedEther.deployed();
   // console.log("WrappedEther deployed to:", wrappedEther.address);
 
-  const provider = new ethers.providers.JsonRpcProvider(
-    process.env.url
-    // "https://kovan.infura.io/v3/<infura_project_id>"
-  );
-  // const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
-  // const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331"
   // const priceFeed = new ethers.Contract(addr, aggregatorV3InterfaceABI, provider)
   // priceFeed.latestRoundData()
   //     .then((roundData) => {
   //         // Do something with roundData
   //         console.log("Latest Round Data", roundData)
   //     })
-  const wrappedEther = new ethers.Contract(
-    "0xa91ca5922c21dfec758f0467cc296b49a31ded35",
-    Wrapped.abi,
-    provider
-  );
-  console.log(wrappedEther);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
